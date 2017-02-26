6857f9212347a4d4c534508fc14e5477335dc571
quietcoolwu <quietcool.wu@gmail.com>
2017-02-26 14:44:15 +0800

    add tf lin_func

--- mlnd/week3/face_recognition.py
+++ mlnd/week3/face_recognition.py
@@ -29,18 +29,16 @@ Gerhard Schroeder       0.76      0.76      0.76        25
 """
 from __future__ import print_function
 
-from time import time
 import logging
-import matplotlib.pyplot as plt
+from time import time
 
+import matplotlib.pyplot as plt
 from sklearn.cross_validation import train_test_split
 from sklearn.datasets import fetch_lfw_people
-from sklearn.grid_search import GridSearchCV
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import RandomizedPCA
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.svm import SVC
-
 
 print(__doc__)
 
