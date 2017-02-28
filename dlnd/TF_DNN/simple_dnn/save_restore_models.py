dc44ec02a2e62d8d67ac77030b740d7568f84ab6
quietcoolwu <quietcool.wu@gmail.com>
2017-03-01 02:50:22 +0800

    finish DNN intro module

--- dlnd/TF_DNN/simple_dnn/save_restore_models.py
+++ dlnd/TF_DNN/simple_dnn/save_restore_models.py
@@ -3,105 +3,128 @@
 import math
 import os
 
-import numpy as np
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 
-class MNISTDNN(object):
-    def __init__():
-        # Hyperparameters setting
+class ModelSaveRestore(object):
+    def __init__(self):
+        # Hyper-parameters' setting
         self.learning_rate = 0.001
         self.n_input = 784
         self.n_classes = 10
         self.batch_size = 128
         self.n_epochs = 100
         self.save_file = './tmp/'
+        # Import MNIST data
         if not os.path.exists(self.save_file):
             os.makedirs(self.save_file)
-        self.saver = tf.train.Saver()
+        self.data = input_data.read_data_sets(
+            os.path.join(self.save_file, 'datasets'), one_hot=True)
 
         # Remove previous Tensors and Operations
         tf.reset_default_graph()
+        print('Static Init Done!')
 
     def model_initialize(self):
-        # Import MNIST data
-        mnist = input_data.read_data_sets(os.path.join(self.save_file, 'ckpt'),
-                                          one_hot=True)
-
+        model_init_flag = False
         # Features and Labels
-        features = tf.placeholder(tf.float32, [None, n_input])
-        labels = tf.placeholder(tf.float32, [None, n_classes])
+        self.features = tf.placeholder(tf.float32, [None, self.n_input])
+        self.labels = tf.placeholder(tf.float32, [None, self.n_classes])
 
-        # Weights & bias
-        weights = tf.Variable(tf.random_normal([n_input, n_classes]))
-        bias = tf.Variable(tf.random_normal([n_classes]))
+        # Weights & bias: name for fine-tuning
+        self.weights = tf.Variable(
+            tf.random_normal([self.n_input, self.n_classes]), name='weights_0')
+        self.bias = tf.Variable(
+            tf.random_normal([self.n_classes]), name='bias_0')
 
         # Logits - xW + b
-        logits = tf.add(tf.matmul(features, weights), bias)
+        logits = tf.add(tf.matmul(self.features, self.weights), self.bias)
 
         # Define loss and optimizer
         cost = tf.reduce_mean(
             tf.nn.softmax_cross_entropy_with_logits(
-                logits=logits, labels=labels))
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)\
-            .minimize(cost)
+                logits=logits, labels=self.labels))
+        self.optimizer = tf.train.GradientDescentOptimizer(
+            learning_rate=self.learning_rate).minimize(cost)
 
         # Calculate accuracy
         correct_prediction = tf.equal(
-            tf.argmax(logits, 1), tf.argmax(labels, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            tf.argmax(logits, 1), tf.argmax(self.labels, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        model_init_flag = not model_init_flag
+        return model_init_flag
 
     def model_train_and_save(self):
+        train_save_flag = False
         # Training initialization:
         init = tf.global_variables_initializer()
-        saver = tf.train.Saver()
-
         # Launch the graph
         with tf.Session() as sess:
             sess.run(init)
-
             # Training cycle
-            for epoch in range(n_epochs):
-                total_batch = math.ceil(mnist.train.num_examples / batch_size)
+            for epoch in range(self.n_epochs):
+                total_batch = math.ceil(self.data.train.num_examples /
+                                        self.batch_size)
 
                 # Loop over all batches
                 for _ in range(total_batch):
-                    batch_features, batch_labels = mnist.train.next_batch(
-                        batch_size)
+                    batch_features, batch_labels = self.data.train.next_batch(
+                        self.batch_size)
                     sess.run(
-                        optimizer,
+                        self.optimizer,
                         feed_dict={
-                            features: batch_features,
-                            labels: batch_labels
+                            self.features: batch_features,
+                            self.labels: batch_labels
                         })
 
                 # Print status for every 10 epochs
                 if epoch % 10 == 0:
                     valid_accuracy = sess.run(
-                        accuracy,
+                        self.accuracy,
                         feed_dict={
-                            features: mnist.validation.images,
-                            labels: mnist.validation.labels
+                            self.features: self.data.validation.images,
+                            self.labels: self.data.validation.labels
                         })
                     print('Epoch {:<3} - Validation Accuracy: {}'.format(
                         epoch, valid_accuracy))
 
             # Save the model in session
-            saver.save(sess, self.save_file)
+            tf.train.Saver().save(sess, self.save_file)
             print('Trained Model Saved.')
 
+        train_save_flag = not train_save_flag
+        return train_save_flag
+
     def model_restore(self):
-        saver = tf.train.Saver()
+        restore_flag = False
         # Launch the graph
         with tf.Session() as sess:
-            saver.restore(sess, save_file)
+            tf.train.Saver().restore(sess, self.save_file)
 
             test_accuracy = sess.run(
-                accuracy,
+                self.accuracy,
                 feed_dict={
-                    features: mnist.test.images,
-                    labels: mnist.test.labels
+                    self.features: self.data.test.images,
+                    self.labels: self.data.test.labels
                 })
 
         print('Test Accuracy: {}'.format(test_accuracy))
+
+        restore_flag = not restore_flag
+        return restore_flag
+
+    @staticmethod
+    def run():
+        case = ModelSaveRestore()
+        model_init = case.model_initialize()
+        assert model_init
+        model_train_save = case.model_train_and_save()
+        assert model_train_save
+        model_restore = case.model_restore()
+        assert model_restore
+
+
+if __name__ == '__main__':
+    ModelSaveRestore.run()
--- dlnd/TF_DNN/simple_dnn/two_layer_relu.py
+++ dlnd/TF_DNN/simple_dnn/two_layer_relu.py
@@ -1,81 +1,125 @@
 #!/usr/bin/env python
 
-import numpy as np
+import math
+import os
+
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 
-def two_layer_network_with_ReLU():
-    mnist = input_data.read_data_sets(
-        './datasets/', one_hot=True, reshape=False)
+class TwoLayerDNNReLU(object):
+    def __init__(self):
+        """
+        A Deep Neural Network with 1 hidden layer(ReLU)
+        """
+        # Parameters
+        self.learning_rate = 0.001
+        self.training_epochs = 20
+        self.batch_size = 128
 
-    # Parameters
-    learning_rate = 0.001
-    training_epochs = 20
-    batch_size = 128
+        # 28 x 28 = 784 pixels
+        self.n_input = 784
+        # 0-9: 10 labels
+        self.n_classes = 10
 
-    # 28 x 28 = 784 pixels
-    n_input = 784
-    # 0-9: 10 labels
-    n_classes = 10
+        # Only one hidden-layer here with 256 neurons
+        self.n_hidden_layer = 256
+        self.save_file = './tmp/'
+        # Import MNIST data
+        if not os.path.exists(self.save_file):
+            os.makedirs(self.save_file)
+        self.data = input_data.read_data_sets(
+            os.path.join(self.save_file, 'datasets'),
+            one_hot=True,
+            reshape=False)
+        # Remove previous Tensors and Operations
+        tf.reset_default_graph()
+        print('Static Init Done!')
 
-    # Only one hidden-layer here with 256 neurons
-    n_hidden_layer = 256
+    def model_initialize(self):
+        model_init_flag = False
+        weights = {
+            'hidden_layer':
+            tf.Variable(tf.random_normal([self.n_input, self.n_hidden_layer])),
+            'out':
+            tf.Variable(
+                tf.random_normal([self.n_hidden_layer, self.n_classes]))
+        }
 
-    weights = {
-        'hidden_layer':
-        tf.Variable(tf.random_normal([n_input, n_hidden_layer])),
-        'out': tf.Variable(tf.random_normal([n_hidden_layer, n_classes]))
-    }
+        biases = {
+            'hidden_layer':
+            tf.Variable(tf.random_normal([self.n_hidden_layer])),
+            'out': tf.Variable(tf.random_normal([self.n_classes]))
+        }
 
-    biases = {
-        'hidden_layer': tf.Variable(tf.random_normal([n_hidden_layer])),
-        'out': tf.Variable(tf.random_normal([n_classes]))
-    }
+        # tf Graph input
+        self.features = tf.placeholder("float", [None, 28, 28, 1])
+        self.labels = tf.placeholder("float", [None, self.n_classes])
+        # like np.reshape
+        features_flat = tf.reshape(self.features, [-1, self.n_input])
 
-    # tf Graph input
-    x = tf.placeholder("float", [None, 28, 28, 1])
-    y = tf.placeholder("float", [None, n_classes])
-    # like np.reshape
-    x_flat = tf.reshape(x, [-1, n_input])
-    """
-    Hidden Layer Start!
-    """
-    # Hidden layer with RELU activation
-    hidden_layer_in = tf.add(
-        tf.matmul(x_flat, weights['hidden_layer']), biases['hidden_layer'])
-    hidden_layer_out = tf.nn.relu(hidden_layer_in)
-    # Output layer with linear activation
-    logits = tf.add(tf.matmul(hidden_layer_out, weights['out']), biases['out'])
+        # Hidden layer with RELU activation
+        hidden_layer_in = tf.add(
+            tf.matmul(features_flat, weights['hidden_layer']),
+            biases['hidden_layer'])
+        hidden_layer_out = tf.nn.relu(hidden_layer_in)
+        # Output layer with linear activation
+        logits = tf.add(
+            tf.matmul(hidden_layer_out, weights['out']), biases['out'])
 
-    # Define loss and optimizer
-    cost = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)\
-        .minimize(cost)
+        # Define loss and optimizer
+        cost = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(
+                logits=logits, labels=self.labels))
+        self.optimizer = tf.train.GradientDescentOptimizer(
+            learning_rate=self.learning_rate).minimize(cost)
 
-    # Initializing the variables
-    init = tf.global_variables_initializer()
+        model_init_flag = not model_init_flag
+        return model_init_flag
 
-    # Launch the graph
-    with tf.Session() as sess:
-        sess.run(init)
-        # Training cycle
-        for _ in range(training_epochs):
-            total_batch = mnist.train.num_examples // batch_size
-            # Loop over all batches
-            for _ in range(total_batch):
-                # next_batch: like an iterator
-                batch_x, batch_y = mnist.train.next_batch(batch_size)
-                # Run optimization (backprop) and cost (to get loss value)
-                """
-                optimizer -> cost -> labels, logits
-                -> ReLU * w + b-> x_flat * w + b -> input
-                """
-                sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-            # batch_x: (128, 28, 28, 1) gray-scale; batch_y: (128, 10)
-            print(batch_x.shape, batch_y.shape)
+    def model_train(self):
+        train_flag = False
+        # Initializing the variables
+        init = tf.global_variables_initializer()
+
+        # Launch the graph
+        with tf.Session() as sess:
+            sess.run(init)
+            # Training cycle
+            for _ in range(self.training_epochs):
+                total_batch = math.ceil(self.data.train.num_examples /
+                                        self.batch_size)
+                # Loop over all batches
+                for _ in range(total_batch):
+                    # next_batch: like an iterator
+                    batch_x, batch_y = self.data.train.next_batch(
+                        self.batch_size)
+                    """
+                    Run optimization (backprop) and cost (to get loss value)
+                    optimizer -> cost -> labels, logits
+                    -> ReLU * w + b-> x_flat * w + b -> input
+                    """
+                    sess.run(
+                        self.optimizer,
+                        feed_dict={
+                            self.features: batch_x,
+                            self.labels: batch_y
+                        })
+                # batch_x: (128, 28, 28, 1) gray-scale; batch_y: (128, 10)
+                print(batch_x.shape, batch_y.shape)
+
+        print('Training ended.')
+        train_flag = not train_flag
+        return train_flag
+
+    @staticmethod
+    def run():
+        case = TwoLayerDNNReLU()
+        model_init = case.model_initialize()
+        assert model_init
+        model_train_save = case.model_train()
+        assert model_train_save
 
 
 if __name__ == '__main__':
-    two_layer_network_with_ReLU()
+    TwoLayerDNNReLU.run()
